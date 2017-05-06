c829ce2c53f1ca3c0ef89a61c96d8bce7bba5cdc
Satoshi SUZUKI <akn@s-satoshi.net>
2017-05-06 12:56:59 +0900

    スクリプト分離の基礎的な構造まで

--- app/game.js
+++ app/game.js
@@ -8,18 +8,14 @@ class Game {
     this.engine = new Engine(renderer, loader);
   }
   async start(){
-    // [wait time=1000]
-    await this.engine.wait({time: 1000});
-    // [image label="bg" storage="everett_effect" opacity=0.1]
-    await this.engine.image({label: "bg", storage:"everett_effect", opacity: 0.1});
-    // [fade label="bg" opacity=0.1 duration=2000]
-    await this.engine.fade({label: "bg", opacity: 1.0, duration: 2000});
-    // [wait time=1000]
-    await this.engine.wait({time: 1000});
-    // [fade label="bg" opacity=0.0 duration=2000]
-    await this.engine.fade({label: "bg", opacity: 0.0, duration: 2000});
-    // [fade label="bg"]
-    await this.engine.remove({label: "bg"});
+    let context = {};
+    // this束縛がアレだなー。 class内のときでもarrowにすればいいのか？
+    context.wait = this.engine.wait.bind(this.engine);
+    context.image = this.engine.image.bind(this.engine);
+    context.remove = this.engine.remove.bind(this.engine);
+    context.fade = this.engine.fade.bind(this.engine);
+
+    Script.call(context);
   }
 }
 
--- app/script.js
+++ app/script.js
@@ -1,6 +1,18 @@
-let Script = function() {
+let Script = async function() {
   "use strict";
-  bgimage("image.png");
+  //TODO: this省略したいなーできないのかなー
+
+  await this.wait({time: 1000});
+  // [image label="bg" storage="everett_effect" opacity=0.1]
+  await this.image({label: "bg", storage:"everett_effect", opacity: 0.1});
+  // [fade label="bg" opacity=0.1 duration=2000]
+  await this.fade({label: "bg", opacity: 1.0, duration: 2000});
+  // [wait time=1000]
+  await this.wait({time: 1000});
+  // [fade label="bg" opacity=0.0 duration=2000]
+  await this.fade({label: "bg", opacity: 0.0, duration: 2000});
+  // [fade label="bg"]
+  await this.remove({label: "bg"});
 };
 
 export default Script;
