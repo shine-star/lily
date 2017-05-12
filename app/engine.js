7634fdc19d2bc4cdcd74b093d9a2314d0725ecd2
Satoshi SUZUKI <akn@s-satoshi.net>
2017-05-12 19:26:04 +0900

    first.jsを実行する処理はinit.jsではなくfirst.jsの管轄にした

--- app/engine.js
+++ app/engine.js
@@ -14,6 +14,7 @@ class Engine {
 
   async run(){
     await this.evaluateSystemAsync('data/system/init.js');
+    await this.runScript("first.js");
   }
 
   async evaluateScript(path){
--- data/system/init.js
+++ data/system/init.js
@@ -3,7 +3,6 @@
   await engine.loadSystem("control.js");
   await engine.loadSystem("graphics.js");
   await engine.loadSystem("text.js");
-  await engine.runScript("first.js");
 })().then(()=>{
   "use strict";
   resolve();
